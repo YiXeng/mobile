6d42bb9ea269eca3f83374b0558a3197015770ac
annali07 <113733207+annali07@users.noreply.github.com>
2023-07-06 22:15:15 +0900

    added output screen

--- App.js
+++ App.js
@@ -3,10 +3,12 @@ import { createStackNavigator } from 'react-navigation-stack';
 
 import ExampleScreen from './src/screens/ExampleScreen';
 import MainScreen from './src/screens/MainScreen';
+import OutputScreen from './src/screens/OutputScreen';
 
 const navigator = createStackNavigator ({
   example: ExampleScreen,
-  main: MainScreen
+  main: MainScreen,
+  output: OutputScreen,
 
 }, {
   initialRouteName: 'main',

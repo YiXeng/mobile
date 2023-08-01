031595589e1478a02955d0ef81f80aedf5f09847
Leo Yin <71328966+leoyin1127@users.noreply.github.com>
2023-08-01 03:49:46 -0400

    Update useCompletion.js

--- src/hooks/useCompletion.js
+++ src/hooks/useCompletion.js
@@ -7,7 +7,10 @@ const useCompletion = (userInput) => {
     const [data, setData] = useState(null);
 
     // Format prompt for travel planning
-    const prompt = `Plan a travel itinerary for ${userInput.lengthOfTour} days to ${userInput.destination}. The budget is ${userInput.budget} and the dates are ${userInput.dates}. The travel focus is ${userInput.preferences}. ${userInput.additionalInfo || ''}`;
+    const prompt = `Plan a travel itinerary for ${userInput.lengthOfTour} 
+                    days to ${userInput.destination}. The budget is ${userInput.budget} 
+                    and the dates are ${userInput.dates}. The travel focus is ${userInput.preferences}. 
+                    ${userInput.additionalInfo || ''}`;
 
     useEffect(() => {
         getCompletion(prompt)

aea3eda4dc4db52c6562a8daa680ede9e3020dc6
annaxzl <annaxzl05@gmail.com>
2023-08-09 23:46:35 -0400

    Added styling for OutputScreen

--- src/components/HourComponent.js
+++ src/components/HourComponent.js
@@ -1,24 +1,23 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { styles } from "../styles/Styles";
 
 export default function HourComponent(props) {
-
   const hourKey = Object.keys(props.hour)[0]; // str hourKey = Hour1
   const hour = props.hour[hourKey]; // obj hour = {time:"xxx", xxx:"xxx", ...}
-  const keys = Object.keys(hour).filter((key) => key !== "time"); 
+  const keys = Object.keys(hour).filter((key) => key !== "time");
 
   return (
-    <View>
-
-      <View>
-        <Text>Time</Text>
-        <Text>{hour.time}</Text>
+    <View style={styles.container}>
+      <View style={{ flexDirection: "row" }}>
+        <Text style={styles.time}>Time</Text>
+        <Text style={styles.time}>{hour.time}</Text>
       </View>
 
       {keys.map((key) => (
-        <View key={key}>
-          <Text>{key}</Text>
-          <Text>{hour[key]}</Text>
+        <View  style={{ flexDirection: "row" }} key={key}>
+          <Text style={styles.time}>{key}</Text>
+          <Text style={styles.time}>{hour[key]}</Text>
         </View>
       ))}
     </View>

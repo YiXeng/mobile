6d49213aa0305f155545df13216245e2d030f824
annaxzl <annaxzl05@gmail.com>
2023-08-23 12:38:58 -0400

    Async Storage function successfully implemented

--- App.js
+++ App.js
@@ -1,22 +1,28 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import ExampleScreen from './src/screens/ExampleScreen';
-import MainScreen from './src/screens/MainScreen';
-import OutputScreen from './src/screens/OutputScreen';
-import UserInputScreen from './src/screens/UserInputScreen';
+import MainScreen from "./src/screens/MainScreen";
+import ExampleScreen from "./src/screens/ExampleScreen";
+import OutputScreen from "./src/screens/OutputScreen";
+import UserInputScreen from "./src/screens/UserInputScreen";
 
-const navigator = createStackNavigator ({
-  example: ExampleScreen,
-  main: MainScreen,
-  output: OutputScreen,
-  input: UserInputScreen
+const Stack = createNativeStackNavigator();
 
-}, {
-  initialRouteName: 'main',
-  defaultNavigationOptions: {
-    headerShown: false
-  }
-});
-
-export default createAppContainer(navigator)
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="main"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="main" component={MainScreen} />
+        <Stack.Screen name="example" component={ExampleScreen} />
+        <Stack.Screen name="output" component={OutputScreen} />
+        <Stack.Screen name="input" component={UserInputScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}

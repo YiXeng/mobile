f5bf69082406dd9158c904a84660bc23ba54a59a
Colin Wu <wzp627@gmail.com>
2023-09-14 23:36:55 -0400

    update usreinputscreen

--- src/screens/UserInputScreen.js
+++ src/screens/UserInputScreen.js
@@ -1,13 +1,8 @@
 import React, { useState } from 'react';
-import { View, TextInput, Text, StyleSheet, Button } from 'react-native';
+import { View, TextInput, Text, StyleSheet, Button, TouchableOpacity } from 'react-native';
+import AddIcon from '@mui/icons-material/Add';
 
 const UserInputScreen = ({ navigation }) => {
-  const [destination, setDestination] = useState("");
-  const [budget, setBudget] = useState("");
-  const [dates, setDates] = useState("");
-  const [preferences, setPreferences] = useState("");
-  const [lengthOfTour, setLengthOfTour] = useState("");
-  const [additionalInfo, setAdditionalInfo] = useState("");
   
   const [destination, setDestination] = useState('');
   const [budget, setBudget] = useState('');
@@ -15,19 +10,23 @@ const UserInputScreen = ({ navigation }) => {
   const [preferences, setPreferences] = useState('');
   const [lengthOfTour, setLengthOfTour] = useState('');
   const [additionalInfo, setAdditionalInfo] = useState('');
+  const [more, setMore] = useState(true)
 
-  const handleSubmit = () => {
+  const addMore = () => {
+    setMore(!more)
+  }
+
+  const handleSubmit = (  ) => {
     const userInput = {
       destination,
       budget,
       dates,
       preferences,
       lengthOfTour,
-      additionalInfo,
+      additionalInfo
     };
     console.log(userInput);
-    handleSaveData();
-    navigation.navigate("output", { key: '1' });
+    navigation.navigate('output');
   };
 
 
@@ -35,38 +34,56 @@ const UserInputScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Text>Destination</Text>
-      <TextInput
-        style={styles.input}
-        value={destination}
-        onChangeText={setDestination}
+      <TextInput 
+        style={styles.input} 
+        value={destination} 
+        onChangeText={setDestination} 
+      />
+      
+      <Text>Budget</Text>
+      <TextInput 
+        style={styles.input} 
+        value={budget} 
+        onChangeText={setBudget} 
+      />
+      
+      <Text>Dates</Text>
+      <TextInput 
+        style={styles.input} 
+        value={dates} 
+        onChangeText={setDates} 
       />
 
-      <Text>Budget</Text>
-      <TextInput style={styles.input} value={budget} onChangeText={setBudget} />
-
-      <Text>Dates</Text>
-      <TextInput style={styles.input} value={dates} onChangeText={setDates} />
-
       <Text>Preferences</Text>
-      <TextInput
-        style={styles.input}
-        value={preferences}
-        onChangeText={setPreferences}
+      <TextInput 
+        style={styles.input} 
+        value={preferences} 
+        onChangeText={setPreferences} 
       />
 
       <Text>Length Of Tour</Text>
-      <TextInput
-        style={styles.input}
-        value={lengthOfTour}
-        onChangeText={setLengthOfTour}
+      <TextInput 
+        style={styles.input} 
+        value={lengthOfTour} 
+        onChangeText={setLengthOfTour} 
       />
 
       <Text>Additional Info</Text>
-      <TextInput
-        style={styles.input}
-        value={additionalInfo}
-        onChangeText={setAdditionalInfo}
+      <TextInput 
+        style={styles.input} 
+        value={additionalInfo} 
+        onChangeText={setAdditionalInfo} 
       />
+
+      {more ? (
+        <TouchableOpacity onPress={addMore} style={styles.moreButton}>
+          <AddIcon />
+          <Text style={styles.moreText}>Add More</Text>
+        </TouchableOpacity>
+      ) : <View>
+        <Text>More Info</Text>
+        <TextInput style={styles.input}/>
+        </View>}
 
       <Button 
         title="Submit" 
@@ -79,12 +96,12 @@ const UserInputScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: 'center',
     paddingHorizontal: 20,
   },
   input: {
     height: 40,
-    borderColor: "gray",
+    borderColor: 'gray',
     borderWidth: 1,
     marginTop: 10,
     marginBottom: 20,

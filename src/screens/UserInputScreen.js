ee15716300c3776a260ac37c9a12d6691306560c
Colin Wu <wzp627@gmail.com>
2023-10-02 16:48:26 -0400

    update userinput

--- src/screens/UserInputScreen.js
+++ src/screens/UserInputScreen.js
@@ -79,40 +79,66 @@ const UserInputScreen = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
+
+      <Text style={styles.tit}>Tell Us About Your Plan</Text>
       
-      <Text>Destination</Text>
+      <View style={styles.bubble}>
+      <Text style={styles.label}>Where is your final destination?</Text>
       <TextInput
         style={styles.input}
         value={destination}
         onChangeText={setDestination}
+        placeholder="Your final destination"
+        placeholderTextColor="#A9A9A9"
       />
+      <View style={styles.inputContainer}></View> 
+      </View>
 
-      <Text>Budget</Text>
-      <TextInput style={styles.input} value={budget} onChangeText={setBudget} />
 
-      <Text>Dates</Text>
-      <TextInput style={styles.input} value={dates} onChangeText={setDates} />
-
-      <Text>Preferences</Text>
-      <TextInput
-        style={styles.input}
-        value={preferences}
-        onChangeText={setPreferences}
+      <View style={styles.bubble}>
+      <Text style={styles.label}>What's your budget?</Text>
+      <TextInput 
+        style={styles.input} 
+        value={budget} 
+        onChangeText={setBudget} 
       />
+      <View style={styles.inputContainer}></View> 
+      </View>
 
-      <Text>Length Of Tour</Text>
+
+
+      <View style={styles.bubble}>
+      <Text style={styles.label}>When do you want your trip started</Text>
+      <TextInput 
+        style={styles.input} 
+        value={dates} 
+        onChangeText={setDates} 
+      />
+      <View style={styles.inputContainer}></View> 
+      </View>
+
+
+
+      <View style={styles.bubble}>
+      <Text style={styles.label}>What's the length of tour?</Text>
       <TextInput
         style={styles.input}
         value={lengthOfTour}
         onChangeText={setLengthOfTour}
       />
+      <View style={styles.inputContainer}></View> 
+      </View>
 
-      <Text>Additional Info</Text>
+
+      <View style={styles.bubble}>
+      <Text style={styles.label}>Any preferences about your trip?</Text>
       <TextInput
         style={styles.input}
-        value={additionalInfo}
-        onChangeText={setAdditionalInfo}
+        value={preferences}
+        onChangeText={setPreferences}
       />
+      <View style={styles.inputContainer}></View> 
+      </View>
 
       <Button title="Submit" onPress={handleSubmit} />
     </View>
@@ -120,19 +146,51 @@ const UserInputScreen = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
+  tit: {
+    margin: 25,
+    justifyContent: "center",
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+
+  label: {
+    alignSelf: 'center',
+    fontSize: 18,
+    marginBottom: 10,
+  },
+
   container: {
     flex: 1,
     justifyContent: "center",
     paddingHorizontal: 20,
   },
-  input: {
-    height: 40,
-    borderColor: "gray",
-    borderWidth: 1,
-    marginTop: 10,
-    marginBottom: 20,
-    paddingLeft: 10,
+
+  inputContainer: {
+    flexDirection: 'row',
+    alignSelf: 'center',
+    alignItems: 'center',
+    margin: 7,
+    borderBottomWidth: 1,
+    borderBottomColor: 'black', // Change the color as needed
+    width: '80%', // Adjust the width as needed
   },
+
+  input: {
+    alignSelf: 'center',
+    alignItems: 'center',
+    paddingVertical: 5,
+    fontSize: 16,
+  },
+
+  bubble: {
+    alignContent: 'center',
+    backgroundColor: 'lightgrey', // Background color for the bubble
+    borderRadius: 10, // Border radius to create rounded corners
+    padding: 10, // Padding inside the bubble
+    marginBottom: 10, // Spacing between bubbles
+    width: '100%', // Adjust the width as needed
+  }
+
 });
 
 export default UserInputScreen;

8c85afb43108102bd29227db6d5d72af064b035e
Zhengyang Wang <zhengyangwang028@gmail.com>
2024-02-25 20:33:30 -0500

    Delete AuthContext.js

--- src/AuthContext.js
+++
@@ -1,9 +0,0 @@
-import React, { Children } from "react";
-
-const AuthContext = React.createContext()
-
-const AuthProvider = ({ children }) => {
-    return <BlogContext.Provider>
-        {children}
-    </BlogContext.Provider>
-}

fa5432fc08b7917402ea1d5454db31f39e4e8a4d
Colin Wu <wzp627@gmail.com>
2024-02-25 20:54:44 -0500

    add AWS user pool

---
+++ aws-exports.js
@@ -0,0 +1,12 @@
+// aws-exports.js
+const awsConfig = {
+    Auth: {
+        region: 'us-east-1', 
+        userPoolId: 'us-east-1_vgVDidKqo',
+        userPoolWebClientId: '应用客户端ID',
+        // 其他配置...
+    },
+    // 其他服务配置...
+};
+
+export default awsConfig;

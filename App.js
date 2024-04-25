3888db9f5a0ffa1977ceed113613dd2be237d2aa
xuan0 <100091998+Xuan0ling@users.noreply.github.com>
2024-04-25 13:57:37 -0400

    1.1

--- App.js
+++ App.js
@@ -53,6 +53,15 @@ function Home() {
         }}
       />
       <Tab.Screen
+        name="NewTravel"
+        component={UserInputScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="plus-circle" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
         name="Community"
         component={Community}
         options={{
